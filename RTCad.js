30770f2ad3aff6d63a567e2635db50b0fbe0fe78
AMYSoft <77925881+amy-soft@users.noreply.github.com>
2022-02-03 21:38:00 +0330

    Update code.

---
+++ RTCad.js
@@ -0,0 +1,42 @@
+class RTCad {
+    constructor() {
+        return "RTCad";
+    }
+
+    async getMedia(constraints) {
+        if (typeof constraints !== "object") {
+            throw "RTCad parameters error!";
+        } else {
+            let stream = await navigator.mediaDevices.getUserMedia(constraints)
+            .then((stream) => {
+                return stream;
+            })
+            .catch((err) => {
+                throw err;
+            });
+        }
+    }
+
+    async getDisplay(constraints) {
+        if (typeof constraints !== "object") {
+            throw "RTCad parameters error!";
+        } else {
+            let stream = await navigator.mediaDevices.getDisplayMedia(constraints)
+            .then((stream) => {
+                return stream;
+            })
+            .catch((err) => {
+                throw err;
+            });
+        }
+    }
+
+    assignTo(element , stream) {
+        var ele = document.querySelector(element);
+        if ("srcObject" in ele) {
+            ele.srcObject = stream;
+        } else {
+            ele.src = window.URL.createObjectURL(stream);
+        }
+    }
+}
